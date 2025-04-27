28e48eac96c9e2654246d4b737ab721d6917fbab
himanshu1k9 <himanshu.develop.24@gmail.com>
2025-04-27 19:20:12 +0530

    added some features of fs module into index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello node js')
+// console.log('hello node js')
+
+/**
+ * ############# FS Modules #############
+ */
+
+const fs = require('fs');
+fs.writeFileSync("test.text", 'Hello node js module fs')
+
+const data = fs.readFileSync('test.text', 'utf-8');
+console.log(data);
+
+fs.writeFile('message.txt', 'hello javascript your awesome features', (err) => 
+    {
+        if(err) throw err;
+        fs.readFile('message.txt', 'utf-8', (err, data) => 
+            {
+                if(err) throw err;
+                console.log(data)
+            });
+    });
