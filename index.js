bc7c5d58cae4420512cf8a888fdda5a096cca99e
himanshu1k9 <himanshu.develop.24@gmail.com>
2025-04-27 19:38:26 +0530

    covered almost important methods of file system

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ fs.writeFile('message.txt', 'hello javascript your awesome features', (err) =>
                 console.log(data)
             });
     });
+
+/**
+ * ############# Commanly used FS Methods ################
+ * 1. fs.writeFile
+ * 2.fs.appendFile :- Add to an existing file
+ * 3. fs.readFile
+ * 4. fs.unlink :- delete a line
+ * 5. fs.rename :- rename an existing file
+ */
+
+// appended text to existing file
+fs.appendFile('message.txt', '\nI have added some other things to the existing one.', (err) => 
+    {
+        if(err) throw err;
+        fs.readFile('message.txt', 'utf-8', (err, data) => 
+            {
+                console.log(data)
+            });
+    });
+
+fs.unlink('test.text', (err) => 
+    {
+        if(err) throw err;
+        console.log('File deleted');
+    });
+
+// fs.rename('message.txt', 'demo.txt', (err) => 
+//     {
+//         if(err) throw err;
+//         console.log('file renamed.')
+//     });
